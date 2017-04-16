b49c661218b301bf2734e8ee70652685947705a6
LiuWeidon <liu18443101163@163.com>
2017-04-16 20:38:28 +0800

    20:38 04/16

---
+++ app/src/main/java/com/example/misaya/testbar/AudioService.java
@@ -0,0 +1,61 @@
+package com.example.misaya.testbar;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+
+public class AudioService extends Service {
+    private MediaPlayer mp;
+    private String query;
+
+    @Override
+    public void onCreate() {
+        //Log.e("Audio","初始化音乐资源");
+        super.onCreate();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        if (query != null && !query.equals(intent.getStringExtra("query")) && mp != null) {
+            mp.start();
+        } else {
+            String query = intent.getStringExtra("query");
+            Uri location = Uri.parse("http://dict.youdao.com/dictvoice?audio=" + query);
+            mp = MediaPlayer.create(this, location);
+            mp.start();
+
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    //向Activity传值 通知播放完毕
+                }
+            });
+
+            mp.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                public boolean onError(MediaPlayer mp, int what, int extra) {
+                    try {
+                        mp.release();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.stop();
+        mp.release();
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
